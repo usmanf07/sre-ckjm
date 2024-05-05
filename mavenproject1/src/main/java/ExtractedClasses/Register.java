663c316e44562f445b329763aca9193ae088b4cb
hassan210302 <hassan210302@gmail.com>
2024-05-06 02:13:26 +0500

    Class Visitor done

--- mavenproject1/src/main/java/ExtractedClasses/Register.java
+++ mavenproject1/src/main/java/ExtractedClasses/Register.java
@@ -6,8 +6,10 @@ package ExtractedClasses;
 
 import gr.spinellis.ckjm.ClassMetrics;
 import gr.spinellis.ckjm.ClassMetricsContainer;
+import gr.spinellis.ckjm.ClassVisitor;
 import gr.spinellis.ckjm.MetricsFilter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.TreeSet;
 import org.apache.bcel.Constants;
@@ -19,37 +21,62 @@ import org.apache.bcel.generic.Type;
  * @author hp
  */
 public class Register {
-    
-    
-    public void registerFieldAccess(String className, String fieldName,String myClassName, ArrayList<TreeSet<String>> mi,HashSet<String> efferentCoupledClasses,ClassMetricsContainer cmap) {
-	registerCoupling(className, myClassName,efferentCoupledClasses,cmap);
-	if (className.equals(myClassName))
-	    mi.get(mi.size() - 1).add(fieldName);
-    }
-    static String className(Type t) {
-	String ts = t.toString();
 
-	if (t.getType() <= Constants.T_VOID) {
-	    return "java.PRIMITIVE";
-	} else if(t instanceof ArrayType) {
-	    ArrayType at = (ArrayType)t;
-	    return className(at.getBasicType());
-	} else {
-	    return t.toString();
-	}
+    private ClassVisitor cv;
+    private HashSet<String> responseSet = new HashSet<String>();
+
+    public Register(ClassVisitor temp) {
+        cv = temp;
     }
-    
-    
-     public void registerCoupling(Type t,String myClassName, ArrayList<TreeSet<String>> mi,HashSet<String> efferentCoupledClasses,ClassMetricsContainer cmap) {
-	registerCoupling(className(t),myClassName,efferentCoupledClasses,cmap);
+    public void registerMethodInvocation(String className, String methodName, Type[] args) {
+       registerCoupling(className);
+        /* Measuring decision: calls to JDK methods are included in the RFC calculation */
+        incRFC(className, methodName, args);
     }
-      public void registerCoupling(String className, String myClassName,HashSet<String> efferentCoupledClasses, ClassMetricsContainer cmap) {
-	/* Measuring decision: don't couple to Java SDK */
-	if ((MetricsFilter.isJdkIncluded() ||
-	     !ClassMetrics.isJdkClass(className)) &&
-	    !myClassName.equals(className)) {
-	    efferentCoupledClasses.add(className);
-	    cmap.getMetrics(className).addAfferentCoupling(myClassName);
-	}
+    public void registerFieldAccess(String className, String fieldName) {
+        registerCoupling(className);
+        if (className.equals(cv.getClassName())) {
+            cv.getMi().get(cv.getMi().size() - 1).add(fieldName);
+        }
     }
+
+    static String className(Type t) {
+        String ts = t.toString();
+
+        if (t.getType() <= Constants.T_VOID) {
+            return "java.PRIMITIVE";
+        } else if (t instanceof ArrayType) {
+            ArrayType at = (ArrayType) t;
+            return className(at.getBasicType());
+        } else {
+            return t.toString();
+        }
+    }
+
+    public void registerCoupling(Type t) {
+        registerCoupling(className(t));
+    }
+
+    public void registerCoupling(String className) {
+        /* Measuring decision: don't couple to Java SDK */
+        if ((MetricsFilter.isJdkIncluded()
+                || !ClassMetrics.isJdkClass(className))
+                && !cv.getClassName().equals(className)) {
+            cv.getEfferentCoupledClasses().add(className);
+            cv.getClassMetricsContainer().getMetrics(className).addAfferentCoupling(cv.getClassName());
+        }
+    }
+
+    public void incRFC(String className, String methodName, Type[] arguments) {
+        String argumentList = Arrays.asList(arguments).toString();
+        // remove [ ] chars from begin and end
+        String args = argumentList.substring(1, argumentList.length() - 1);
+        String signature = className + "." + methodName + "(" + args + ")";
+        responseSet.add(signature);
+    }
+
+    public int responseSize() {
+        return responseSet.size();
+    }
+
 }
--- mavenproject1/src/main/java/gr/spinellis/ckjm/MethodVisitor.java
+++ mavenproject1/src/main/java/gr/spinellis/ckjm/MethodVisitor.java
@@ -29,7 +29,7 @@ import java.util.*;
  * @version $Revision: 1.8 $
  * @author <a href="http://www.spinellis.gr">Diomidis Spinellis</a>
  */
-class MethodVisitor extends EmptyVisitor {
+public class MethodVisitor extends EmptyVisitor {
     /** Method generation template. */
     private MethodGen mg;
     /* The class's constant pool. */
@@ -40,11 +40,11 @@ class MethodVisitor extends EmptyVisitor {
     private ClassMetrics cm;
 
     /** Constructor. */
-    MethodVisitor(MethodGen m, ClassVisitor c) {
+    public MethodVisitor(MethodGen m, ClassVisitor c) {
 	mg  = m;
 	cv = c;
 	cp  = mg.getConstantPool();
-	cm = cv.getMetrics();
+	cm = cv.getClassMetrics();
     }
 
     /** Start the method's visit. */
@@ -73,43 +73,43 @@ class MethodVisitor extends EmptyVisitor {
     /** Local variable use. */
     public void visitLocalVariableInstruction(LocalVariableInstruction i) {
 	if(i.getOpcode() != Constants.IINC)
-	    cv.registercall(i.getType(cp));
+	    cv.getRegister().registerCoupling(i.getType(cp));
     }
 
     /** Array use. */
     public void visitArrayInstruction(ArrayInstruction i) {
-	cv.registercall(i.getType(cp));
+	cv.getRegister().registerCoupling(i.getType(cp));
     }
 
     /** Field access. */
     public void visitFieldInstruction(FieldInstruction i) {
-	cv.registerFieldAccessCaller(i.getClassName(cp), i.getFieldName(cp));
-	cv.registercall(i.getFieldType(cp));
+	cv.getRegister().registerFieldAccess(i.getClassName(cp), i.getFieldName(cp));
+	cv.getRegister().registerCoupling(i.getFieldType(cp));
     }
 
     /** Method invocation. */
     public void visitInvokeInstruction(InvokeInstruction i) {
 	Type[] argTypes   = i.getArgumentTypes(cp);
 	for (int j = 0; j < argTypes.length; j++)
-	    cv.registercall(argTypes[j]);
-	cv.registercall(i.getReturnType(cp));
+	    cv.getRegister().registerCoupling(argTypes[j]);
+	cv.getRegister().registerCoupling(i.getReturnType(cp));
 	/* Measuring decision: measure overloaded methods separately */
-	cv.registerMethodInvocation(i.getClassName(cp), i.getMethodName(cp), argTypes);
+	cv.getRegister().registerMethodInvocation(i.getClassName(cp), i.getMethodName(cp), argTypes);
     }
 
     /** Visit an instanceof instruction. */
     public void visitINSTANCEOF(INSTANCEOF i) {
-	cv.registercall(i.getType(cp));
+	cv.getRegister().registerCoupling(i.getType(cp));
     }
 
     /** Visit checklast instruction. */
     public void visitCHECKCAST(CHECKCAST i) {
-	cv.registercall(i.getType(cp));
+	cv.getRegister().registerCoupling(i.getType(cp));
     }
 
     /** Visit return instruction. */
     public void visitReturnInstruction(ReturnInstruction i) {
-	cv.registercall(i.getType(cp));
+	cv.getRegister().registerCoupling(i.getType(cp));
     }
 
     /** Visit the method's exception handlers. */
@@ -120,7 +120,7 @@ class MethodVisitor extends EmptyVisitor {
 	for(int i=0; i < handlers.length; i++) {
 	    Type t = handlers[i].getCatchType();
 	    if (t != null)
-		cv.registercall(t);
+		cv.getRegister().registerCoupling(t);
 	}
     }
 }
