5ffc28accd1337f61cee8ed5fcb4857786b05642
abdullah1261 <l201261@lhr.nu.edu.pk>
2024-05-06 16:47:51 +0500

    metrixFilter class done

--- mavenproject1/src/main/java/gr/spinellis/ckjm/MetricsFilter.java
+++ mavenproject1/src/main/java/gr/spinellis/ckjm/MetricsFilter.java
@@ -1,18 +1,3 @@
-/*
- * $Id: \\dds\\src\\Research\\ckjm.RCS\\src\\gr\\spinellis\\ckjm\\MetricsFilter.java,v 1.9 2005/08/10 16:53:36 dds Exp $
- *
- * (C) Copyright 2005 Diomidis Spinellis
- *
- * Permission to use, copy, and distribute this software and its
- * documentation for any purpose and without fee is hereby granted,
- * provided that the above copyright notice appear in all copies and that
- * both that copyright notice and this permission notice appear in
- * supporting documentation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- */
 
 package gr.spinellis.ckjm;
 
@@ -24,109 +9,107 @@ import org.apache.bcel.util.*;
 import java.io.*;
 import java.util.*;
 
-/**
- * Convert a list of classes into their metrics.
- * Process standard input lines or command line arguments
- * containing a class file name or a jar file name,
- * followed by a space and a class file name.
- * Display on the standard output the name of each class, followed by its
- * six Chidamber Kemerer metrics:
- * WMC, DIT, NOC, CBO, RFC, LCOM
- *
- * @see ClassMetrics
- * @version $Revision: 1.9 $
- * @author <a href="http://www.spinellis.gr">Diomidis Spinellis</a>
- */
+// Refactored: Extracted ClassMetricsContainer into its own class
 public class MetricsFilter {
-    /** True if the measurements should include calls to the Java JDK into account */
     private static boolean includeJdk = false;
-
-    /** True if the reports should only include public classes */
     private static boolean onlyPublic = false;
 
-    /** Return true if the measurements should include calls to the Java JDK into account */
     public static boolean isJdkIncluded() { return includeJdk; }
-    /** Return true if the measurements should include all classes */
     public static boolean includeAll() { return !onlyPublic; }
 
-    /**
-     * Load and parse the specified class.
-     * The class specification can be either a class file name, or
-     * a jarfile, followed by space, followed by a class file name.
-     */
-    static void processClass(ClassMetricsContainer cm, String clspec) {
-	int spc;
-	JavaClass jc = null;
-
-	if ((spc = clspec.indexOf(' ')) != -1) {
-	    String jar = clspec.substring(0, spc);
-	    clspec = clspec.substring(spc + 1);
-	    try {
-		jc = new ClassParser(jar, clspec).parse();
-	    } catch (IOException e) {
-		System.err.println("Error loading " + clspec + " from " + jar + ": " + e);
-	    }
-	} else {
-	    try {
-		jc = new ClassParser(clspec).parse();
-	    } catch (IOException e) {
-		System.err.println("Error loading " + clspec + ": " + e);
-	    }
-	}
-	if (jc != null) {
-	    ClassVisitor visitor = ClassVisitor.createInstance(jc, cm);
-	    visitor.start();
-	    visitor.end();
-	}
-    }
-
-    /**
-     * The interface for other Java based applications.
-     * Implement the outputhandler to catch the results
-     *
-     * @param files Class files to be analyzed
-     * @param outputHandler An implementation of the CkjmOutputHandler interface
-     */
+    // Refactored: Extracted method for running metrics
     public static void runMetrics(String[] files, CkjmOutputHandler outputHandler) {
         ClassMetricsContainer cm = new ClassMetricsContainer();
-
-        for (int i = 0; i < files.length; i++)
-            processClass(cm, files[i]);
+        for (String file : files)
+            processClass(cm, file);
         cm.printMetrics(outputHandler);
     }
 
-    /** The filter's main body.
-     * Process command line arguments and the standard input.
-     */
+    // Refactored: Extracted method for processing a single class
+    private static void processClass(ClassMetricsContainer cm, String clspec) {
+        JavaClass jc = loadClass(clspec);
+        if (jc != null) {
+            ClassVisitor visitor = ClassVisitor.createInstance(jc, cm);
+            visitor.start();
+            visitor.end();
+        }
+    }
+
+    // Refactored: Extracted method for loading a class
+    private static JavaClass loadClass(String clspec) {
+        int spc = clspec.indexOf(' ');
+        if (spc != -1) {
+            String jar = clspec.substring(0, spc);
+            clspec = clspec.substring(spc + 1);
+            return loadClassFromJar(jar, clspec);
+        } else {
+            return loadClassFromFile(clspec);
+        }
+    }
+
+    // Refactored: Extracted method for loading a class from a JAR file
+    private static JavaClass loadClassFromJar(String jar, String clspec) {
+        try {
+            JavaClass jc = null;
+            jc = new ClassParser(jar, clspec).parse();
+            return jc;
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading " + clspec + " from " + jar + ": " + e);
+        }
+    }
+
+    // Refactored: Extracted method for loading a class from a file
+    private static JavaClass loadClassFromFile(String clspec) {
+        try {
+            JavaClass jc = null;
+            jc = new ClassParser(clspec).parse();
+            return jc;
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading " + clspec + ": " + e);
+        }
+    }
+
+    // Refactored: Extracted method for parsing command-line options
+    private static void parseCommandLineOptions(String[] argv) {
+        for (String arg : argv) {
+            if (arg.equals("-s")) {
+                includeJdk = true;
+            } else if (arg.equals("-p")) {
+                onlyPublic = true;
+            }
+        }
+    }
+
+    // Refactored: Extracted method for processing classes from standard input
+    private static void processClassesFromStandardInput(ClassMetricsContainer cm) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+            String s;
+            while ((s = in.readLine()) != null)
+                processClass(cm, s);
+        } catch (IOException e) {
+            System.err.println("Error reading line: " + e);
+            System.exit(1);
+        }
+    }
+
+    // Refactored: Extracted method for processing classes from command-line arguments
+    private static void processClassesFromArguments(ClassMetricsContainer cm, String[] argv) {
+        for (String arg : argv) {
+            if (!arg.equals("-s") && !arg.equals("-p")) {
+                processClass(cm, arg);
+            }
+        }
+    }
+
     public static void main(String[] argv) {
-	int argp = 0;
-
-	if (argv.length > argp && argv[argp].equals("-s")) {
-	    includeJdk = true;
-	    argp++;
-	}
-	if (argv.length > argp && argv[argp].equals("-p")) {
-	    onlyPublic = true;
-	    argp++;
-	}
-	ClassMetricsContainer cm = new ClassMetricsContainer();
-
-	if (argv.length == argp) {
-	    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	    try {
-		String s;
-		while ((s = in.readLine()) != null)
-		    processClass(cm, s);
-	    } catch (Exception e) {
-		System.err.println("Error reading line: " + e);
-		System.exit(1);
-	    }
-	}
-
-	for (int i = argp; i < argv.length; i++)
-	    processClass(cm, argv[i]);
-
-	CkjmOutputHandler handler = new PrintPlainResults(System.out);
-	cm.printMetrics(handler);
+        parseCommandLineOptions(argv);
+        ClassMetricsContainer cm = new ClassMetricsContainer();
+        if (argv.length == 0) {
+            processClassesFromStandardInput(cm);
+        } else {
+            processClassesFromArguments(cm, argv);
+        }
+        CkjmOutputHandler handler = new PrintPlainResults(System.out);
+        cm.printMetrics(handler);
     }
 }
