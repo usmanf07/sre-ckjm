346dad2e849937c6eec3752bb67c29e3eec36871
abdullah1261 <l201261@lhr.nu.edu.pk>
2024-05-12 19:04:55 +0500

    ClassMetricsContainer done

--- mavenproject1/src/main/java/gr/spinellis/ckjm/ClassMetricsContainer.java
+++ mavenproject1/src/main/java/gr/spinellis/ckjm/ClassMetricsContainer.java
@@ -1,30 +1,14 @@
-/*
- * $Id: \\dds\\src\\Research\\ckjm.RCS\\src\\gr\\spinellis\\ckjm\\ClassMetricsContainer.java,v 1.9 2005/08/10 16:53:36 dds Exp $
- *
- * (C) Copyright 2005 Diomidis Spinellis
- *
- * Permission to use, copy, and distribute this software and its
- * documentation for any purpose and without fee is hereby granted,
- * provided that the above copyright notice appear in all copies and that
- * both that copyright notice and this permission notice appear in
- * supporting documentation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- */
-
 package gr.spinellis.ckjm;
 
-import org.apache.bcel.classfile.*;
-import java.util.*;
-import java.io.*;
-
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * A container of class metrics mapping class names to their metrics.
- * This class contains the the metrics for all class's during the filter's
- * operation.  Some metrics need to be updated as the program processes
+ * This class contains the metrics for all classes during the filter's
+ * operation. Some metrics need to be updated as the program processes
  * other classes, so the class's metrics will be recovered from this
  * container to be updated.
  *
@@ -32,30 +16,28 @@ import java.io.*;
  * @author <a href="http://www.spinellis.gr">Diomidis Spinellis</a>
  */
 public class ClassMetricsContainer {
-
     /** The map from class names to the corresponding metrics */
-    private HashMap<String, ClassMetrics> m = new HashMap<String, ClassMetrics>();
+    private final Map<String, ClassMetrics> metricsMap = new HashMap<>();
 
-    /** Return a class's metrics */
+    /**
+     * Return a class's metrics.
+     * If the metrics don't exist, create new metrics and add them to the map.
+     */
     public ClassMetrics getMetrics(String name) {
-	ClassMetrics cm = m.get(name);
-	if (cm == null) {
-	    cm = new ClassMetrics();
-	    m.put(name, cm);
-	}
-	return cm;
+        return metricsMap.computeIfAbsent(name, key -> new ClassMetrics());
     }
 
-    /** Print the metrics of all the visited classes. */
+    /**
+     * Print the metrics of all the visited classes.
+     * Only print metrics for classes that are visited and meet the inclusion criteria.
+     */
     public void printMetrics(CkjmOutputHandler handler) {
-	Set<Map.Entry<String, ClassMetrics>> entries = m.entrySet();
-	Iterator<Map.Entry<String, ClassMetrics>> i;
-
-	for (i = entries.iterator(); i.hasNext(); ) {
-	    Map.Entry<String, ClassMetrics> e = i.next();
-	    ClassMetrics cm = e.getValue();
-	    if (cm.isVisited() && (MetricsFilter.includeAll() || cm.isPublic()))
-		handler.handleClass(e.getKey(), cm);
-	}
+        for (Map.Entry<String, ClassMetrics> entry : metricsMap.entrySet()) {
+            String className = entry.getKey();
+            ClassMetrics classMetrics = entry.getValue();
+            if (classMetrics.isVisited() && (MetricsFilter.includeAll() || classMetrics.isPublic())) {
+                handler.handleClass(className, classMetrics);
+            }
+        }
     }
 }
